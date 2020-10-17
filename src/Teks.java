d4cea4831db7fbbffde148edf5d56fe993a86caf
diky <diky@diky-PC>
2020-10-17 13:57:28 +0700

    Upload Project

---
+++ src/Teks.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * NAMA  : Diva Sabila Ramadhan '
+ * Kelas : IF1
+ * NIM   : 10119039
+ * Deskeripsi Program : Program pecahan 
+ */
+public class Teks {
+
+    
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\t';
+        c = 'A';
+        System.out.println("Hasil" + newline + " Char " + " berisi " + c);
+    }
+    
+}
